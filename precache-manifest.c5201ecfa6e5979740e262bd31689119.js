0a78f3d50b88c0c099251e93a8611b850a1e5d8b
prince-09 <princesinha7072@gmail.com>
2021-06-14 11:17:40 +0530

    Updates

---
+++ precache-manifest.c5201ecfa6e5979740e262bd31689119.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe2163197b1c6a73d6166dd5dd126b41",
+    "url": "/CoronaTrackerWebApp/index.html"
+  },
+  {
+    "revision": "068d9bdce48b48b63e58",
+    "url": "/CoronaTrackerWebApp/static/css/main.15d221f8.chunk.css"
+  },
+  {
+    "revision": "327901e4de5b54e8da92",
+    "url": "/CoronaTrackerWebApp/static/js/2.bc1f5b0b.chunk.js"
+  },
+  {
+    "revision": "5e5c5aaefd726bfc2ad51722d14c5d57",
+    "url": "/CoronaTrackerWebApp/static/js/2.bc1f5b0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068d9bdce48b48b63e58",
+    "url": "/CoronaTrackerWebApp/static/js/main.943324ac.chunk.js"
+  },
+  {
+    "revision": "b3849546e6268f48652d",
+    "url": "/CoronaTrackerWebApp/static/js/runtime-main.209b33d1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CoronaTrackerWebApp/static/media/image.d7265326.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02f056b1ac92b03002f7b7cab69d0214.js"
+  "/CoronaTrackerWebApp/precache-manifest.c5201ecfa6e5979740e262bd31689119.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CoronaTrackerWebApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
