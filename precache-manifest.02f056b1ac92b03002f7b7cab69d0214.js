bccd2cd863c5610582fe772fc75a331ab6d7ccf1
prince-09 <princesinha7072@gmail.com>
2021-06-14 11:03:47 +0530

    Updates

---
+++ precache-manifest.02f056b1ac92b03002f7b7cab69d0214.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739bf25f02e9f183f99532fb2c9c4295",
+    "url": "/index.html"
+  },
+  {
+    "revision": "068d9bdce48b48b63e58",
+    "url": "/static/css/main.15d221f8.chunk.css"
+  },
+  {
+    "revision": "e4fbc44ce36cb13ad388",
+    "url": "/static/js/2.f22da952.chunk.js"
+  },
+  {
+    "revision": "5e5c5aaefd726bfc2ad51722d14c5d57",
+    "url": "/static/js/2.f22da952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "068d9bdce48b48b63e58",
+    "url": "/static/js/main.943324ac.chunk.js"
+  },
+  {
+    "revision": "a5ddf7359d780a23adaa",
+    "url": "/static/js/runtime-main.9866c732.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
